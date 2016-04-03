1a6288774179a80ca376570b4efd2a26775985db
Paul Wallace <prwallac@ucsd.edu>
2016-04-02 21:21:46 -0700

    Will work on the menu tomorrow and then work on implementing the actual menu itmes

--- OpenSourceVoting/MenuView.swift
+++ OpenSourceVoting/MenuView.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
+let NUMBER_OF_OPTIONS = 8
+
 class MenuView: UIView {
 
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.frame = frame
+        
+        
+        
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
